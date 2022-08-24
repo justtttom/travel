7d6455983c1781d46e9d9f357d64dd81d7479dc6
Justin Tom <2541198816@qq.com>
2022-08-24 20:27:26 +0800

    rest

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import styles from './App.module.css';
-import {Header,Footer} from './components'
+import { Header, Footer } from './components';
+import { Row, Col } from 'antd';
 
 
 function App() {
@@ -9,7 +10,14 @@ function App() {
       <Header />
       {/* 页面内容 content */}
       <div className={styles['page-content']}>
-        
+        <Row style={{ marginTop: 20 }}>
+          <Col span={6}>
+            <div style={{ background: "yellow" }}>多重菜单</div>
+          </Col>
+          <Col span={18}>
+            <div style={{ background: "green" }}>跑马灯</div>
+          </Col>
+        </Row>
       </div>
       <Footer />
     </div >
