e842f9bede9225c39e8258b33a2e90e1d16d3221
Justin Tom <2541198816@qq.com>
2022-08-24 21:28:50 +0800

    sideMenu

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import styles from './App.module.css';
-import { Header, Footer } from './components';
+import { Header, Footer,sideMenu } from './components';
 import { Row, Col } from 'antd';
 
 
@@ -12,7 +12,7 @@ function App() {
       <div className={styles['page-content']}>
         <Row style={{ marginTop: 20 }}>
           <Col span={6}>
-            <div style={{ background: "yellow" }}>多重菜单</div>
+            <div style={{ background: "yellow" }}><sideMenu></div>
           </Col>
           <Col span={18}>
             <div style={{ background: "green" }}>跑马灯</div>
