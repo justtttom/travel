7ada888d683d697329b70dfb6d1cffb2215c82f3
Justin Tom <2541198816@qq.com>
2022-08-24 20:59:13 +0800

    mock data

---
+++ src/components/sideMenu/sideMenu.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import styles from './sideMenu.module.css';
+import { sideMenuList } from './mockup';
+import { Menu } from 'antd';
+import {GifOutlined} from '@ant-design/icons';
+
+
+export const sideMenu: React.FC = () => {
+  return ()
+};
