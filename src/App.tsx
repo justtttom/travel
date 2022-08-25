75d07b81732cabbea31b4aa4c79c68f9df9b5278
Justin Tom <2541198816@qq.com>
2022-08-25 23:03:30 +0800

    business partners

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import styles from './App.module.css';
-import { Header, Footer, Carousel,SideMenu,ProductCollection } from './components';
+import { Header, Footer, Carousel, SideMenu, ProductCollection } from './components';
 import { Row, Col, Typography } from 'antd';
-import {productList1,productList2,productList3} from './mockup';
+import { productList1, productList2, productList3 } from './mockup';
 import sideImage from './assets/images/sider_2019_12-09.png'
 import sideImage2 from './assets/images/sider_2019_02-04.png'
 import sideImage3 from './assets/images/sider_2019_02-04-2.png'
@@ -16,26 +16,26 @@ function App() {
       <div className={styles['page-content']}>
         <Row style={{ marginTop: 20 }}>
           <Col span={6}>
-          <SideMenu />
+            <SideMenu />
           </Col>
           <Col span={18}>
             <Carousel />
           </Col>
         </Row>
         <ProductCollection
-        title={<Typography.Title level={3} type="warning">爆款推荐</Typography.Title>}
-        sideImage={sideImage}
-        products={productList1}
+          title={<Typography.Title level={3} type="warning">爆款推荐</Typography.Title>}
+          sideImage={sideImage}
+          products={productList1}
         />
         <ProductCollection
-        title={<Typography.Title level={3} type="danger">新品推荐</Typography.Title>}
-        sideImage={sideImage2}
-        products={productList2}
+          title={<Typography.Title level={3} type="danger">新品上市</Typography.Title>}
+          sideImage={sideImage2}
+          products={productList2}
         />
         <ProductCollection
-        title={<Typography.Title level={2} type="success">国内游推荐</Typography.Title>}
-        sideImage={sideImage3}
-        products={productList3}
+          title={<Typography.Title level={3} type="success">新品上市</Typography.Title>}
+          sideImage={sideImage3}
+          products={productList3}
         />
       </div>
       <Footer />
