1ef55ed61113dfe8ac4a76c7c70f33e210f402c7
Justin Tom <2541198816@qq.com>
2022-08-26 21:39:38 +0800

    router learn

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,5 @@
 import React from 'react';
 import styles from './App.module.css';
-import { Header, Footer, Carousel, SideMenu, ProductCollection, BusinessPartners } from './components';
-import { Row, Col, Typography } from 'antd';
-import { productList1, productList2, productList3 } from './mockup';
-import sideImage from './assets/images/sider_2019_12-09.png'
-import sideImage2 from './assets/images/sider_2019_02-04.png'
-import sideImage3 from './assets/images/sider_2019_02-04-2.png'
 
 
 function App() {
---
+++ src/pages/home/HomePage.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import styles from './HomePage.module.css';
+import { Header, Footer, Carousel, SideMenu, ProductCollection, BusinessPartners } from './components';
+import { Row, Col, Typography } from 'antd';
+import { productList1, productList2, productList3 } from './pages/home/mockup';
+import sideImage from './assets/images/sider_2019_12-09.png'
+import sideImage2 from './assets/images/sider_2019_02-04.png'
+import sideImage3 from './assets/images/sider_2019_02-04-2.png'
+
+
+export class HomePage extends React.Component{
+  return() {
+    return <></>
+  }
+}
