913d0498c815e7deaa098f73107cd10a3dfb1c43
Justin Tom <2541198816@qq.com>
2022-08-26 21:57:32 +0800

    install react router dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import styles from './App.module.css';
+import { HomePage } from '../src/pages'
 
 
 function App() {
   return (
     <div className={styles.App}>
+      <HomePage />
     </div >
   );
 }
